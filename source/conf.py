bdd6d7aa88b57fc7239589d214918d3bd2b97b65
zhaojiedi <zhaojiedi@bytedance.com>
2022-04-17 23:53:32 +0800

    cm

--- source/conf.py
+++ source/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx_sitemap',
+    'sphinx-copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
